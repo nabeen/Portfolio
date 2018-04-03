a08d8f395227d0aa511ff12eb84c928b0f354828
nabeen <watanabe_kenichiro@hasigo.co.jp>
2018-04-03 22:31:34 +0900

    mod config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Dimension by HTML5 UP"
+    title: "nabeen.be",
+    author: "nabeen",
+    description: "nabeen's Portfolio Site"
   },
   pathPrefix: '/',
   plugins: [
